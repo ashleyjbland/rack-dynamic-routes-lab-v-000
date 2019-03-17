fc0a0692d40691143ebbdc0a6f816319ecd10aa8
Ashley Bland <ashleyjbland@gmail.com>
2019-03-17 17:40:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,13 @@
 class Application
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if reg.path.match(/items/)
+      resp.write Item.name
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
   end
 end
